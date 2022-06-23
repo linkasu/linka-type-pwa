4a82de61457a3d0a87374d3a43a9987624e09108
Ivan Bakaidov <ivan@aacidov.ru>
2022-06-23 12:28:28 +0300

    add file download func

--- src/blocks/MainUI.vue
+++ src/blocks/MainUI.vue
@@ -12,8 +12,8 @@
     <tutorial
       v-if="tutorialMode"
       @close="
-        tutorialMode = false;
-        lc.setBoolean('tutorial', false);
+        tutorialMode = false
+        lc.setBoolean('tutorial', false)
       "
     />
     <shortcut-list v-if="shortcutMode" @close="shortcutMode = false" />
@@ -27,9 +27,21 @@
           @showMode="showMode = $event"
         />
 
-        <v-btn block @click="say">{{
-          playing ? "Остановить" : "Сказать"
-        }}</v-btn>
+        <v-layout>
+          <v-row>
+            <v-col :sm="isYandex?11:12">
+              <v-btn block @click="say">
+                {{ playing ? 'Остановить' : 'Сказать' }}
+              </v-btn>
+            </v-col>
+
+            <v-col sm="1" v-if="isYandex">
+              <v-btn block @click="()=>say(true)">
+                <v-icon color="">mdi-download</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-layout>
       </v-card-text>
       <v-card-text>
         <quickes v-if="isQuickes" />
@@ -42,21 +54,21 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Component from "vue-class-component";
+import Vue from 'vue'
+import Component from 'vue-class-component'
 
-import TTS from "../lib/TTS";
+import TTS from '../lib/TTS'
 
-import LHeader from "./LHeader.vue";
-import Bank from "./Bank.vue";
-import Tutorial from "./Tutorial.vue";
-import Quickes from "./Quickes.vue";
-import Settings from "./Settings.vue";
-import MainInput from "./components/MainInput.vue";
-import ShortcutList from "./ShortcutList.vue";
-import LocalMemory from "../lib/LocalMemory";
-import { Watch } from "vue-property-decorator";
-import { analytics } from "firebase";
+import LHeader from './LHeader.vue'
+import Bank from './Bank.vue'
+import Tutorial from './Tutorial.vue'
+import Quickes from './Quickes.vue'
+import Settings from './Settings.vue'
+import MainInput from './components/MainInput.vue'
+import ShortcutList from './ShortcutList.vue'
+import LocalMemory from '../lib/LocalMemory'
+import { Watch } from 'vue-property-decorator'
+import { analytics } from 'firebase'
 
 @Component({
   components: {
@@ -70,73 +82,76 @@ import { analytics } from "firebase";
   },
 })
 export default class MainUI extends Vue {
-  lc = LocalMemory.instance;
-  textForSpeak: string[] = ["", "", ""];
-  showMode: boolean = false;
-  settingsMode = false;
-  shortcutMode = false;
-  isQuickes = true;
-  isBank = true;
-  tutorialMode = this.lc.getBoolean("tutorial", true);
+  lc = LocalMemory.instance
+  textForSpeak: string[] = ['', '', '']
+  showMode: boolean = false
+  settingsMode = false
+  shortcutMode = false
+  isQuickes = true
+  isBank = true
+  tutorialMode = this.lc.getBoolean('tutorial', true)
 
-  chat = 0;
-  playing: boolean = false;
+  chat = 0
+  playing: boolean = false
 
   mounted() {
-    TTS.instance.events.on("start", () => {
-      this.playing = true;
-    });
-    TTS.instance.events.on("end", () => {
-      this.playing = false;
-    });
+    TTS.instance.events.on('start', () => {
+      this.playing = true
+    })
+    TTS.instance.events.on('end', () => {
+      this.playing = false
+    })
   }
 
-  @Watch("settingsMode") onSettingsMode(value: boolean) {
+  @Watch('settingsMode') onSettingsMode(value: boolean) {
     if (!value) {
-      this.isQuickes = !!this.lc.getBoolean("quickes", true);
-      this.isBank = !!this.lc.getBoolean("bank", true);
+      this.isQuickes = !!this.lc.getBoolean('quickes', true)
+      this.isBank = !!this.lc.getBoolean('bank', true)
     }
   }
-  say() {
-    analytics().logEvent("say");
-    const saveOnSay = !!LocalMemory.instance.getBoolean("saveOnSay", false);
-    const text = this.textForSpeak[this.chat];
+  say(download = false) {
+    analytics().logEvent('say')
+    const saveOnSay = !!LocalMemory.instance.getBoolean('saveOnSay', false)
+    const text = this.textForSpeak[this.chat]
     if (saveOnSay) {
-      const bank = this.$refs.bank as Bank;
-      bank.createStatement(text);
+      const bank = this.$refs.bank as Bank
+      bank.createStatement(text)
     }
     if (this.playing) {
-      TTS.instance.stop();
-      this.playing = false;
+      TTS.instance.stop()
+      this.playing = false
     } else {
-      TTS.instance.say(text);
+      TTS.instance.say(text, download)
     }
   }
   paste(event: string) {
-    this.textForSpeak[this.chat] = this.textForSpeak[this.chat] + " " + event;
+    this.textForSpeak[this.chat] = this.textForSpeak[this.chat] + ' ' + event
 
     //dont ask, it needs
-    this.chat++;
+    this.chat++
     setTimeout(() => {
-      this.chat--;
-    }, 0);
+      this.chat--
+    }, 0)
   }
   windowInput(event: KeyboardEvent) {
     if (event.metaKey || event.ctrlKey) {
       if (event.keyCode === 38) {
-        this.chat = this.chat === 0 ? 2 : this.chat - 1;
+        this.chat = this.chat === 0 ? 2 : this.chat - 1
       } else if (event.keyCode === 40) {
-        this.chat = this.chat === 2 ? 0 : this.chat + 1;
+        this.chat = this.chat === 2 ? 0 : this.chat + 1
       }
     }
   }
   created() {
-    const lc = LocalMemory.instance;
-    this.isQuickes = !!lc.getBoolean("quickes", true);
-    this.isBank = !!lc.getBoolean("bank", true);
-    window.addEventListener("keydown", this.windowInput);
+    const lc = LocalMemory.instance
+    this.isQuickes = !!lc.getBoolean('quickes', true)
+    this.isBank = !!lc.getBoolean('bank', true)
+    window.addEventListener('keydown', this.windowInput)
 
-    console.log(this);
+    console.log(this)
+  }
+  get isYandex(){
+    return TTS.instance.yandex
   }
 }
 </script>
--- src/lib/TTS.ts
+++ src/lib/TTS.ts
@@ -1,71 +1,83 @@
-import LocalMemory from './LocalMemory';
-import { analytics } from 'firebase';
-import axios from 'axios';
-import { EventEmitter } from 'events';
+import LocalMemory from './LocalMemory'
+import { analytics } from 'firebase'
+import axios from 'axios'
+import { EventEmitter } from 'events'
 
 class TTS {
-  private synth: SpeechSynthesis;
-  private voice: SpeechSynthesisVoice | YandexVoice | null = null;
-  private storage = new LocalMemory();
-  private audio = new Audio();
+  private synth: SpeechSynthesis
+  private voice: SpeechSynthesisVoice | YandexVoice | null = null
+  private storage = new LocalMemory()
+  private audio = new Audio()
   public events = new EventEmitter()
   public static get instance(): TTS {
-    if (instance == undefined) instance = new TTS
-    return instance;
+    if (instance == undefined) instance = new TTS()
+    return instance
   }
   constructor() {
     if (window.speechSynthesis == null) {
-
-      this.yandex = true;
+      this.yandex = true
     }
-    this.synth = window.speechSynthesis;
+    this.synth = window.speechSynthesis
   }
-  say(text: string) {
-    if (text !== "") {
-
-      var utterThis = new SpeechSynthesisUtterance(text);
+  say(text: string, download = false) {
+    if (text !== '') {
+      var utterThis = new SpeechSynthesisUtterance(text)
       if (this.voice == null) {
-        this.voice = this.selectedVoice;
-
+        this.voice = this.selectedVoice
       }
       this.events.emit('start')
       if (this.yandex) {
-        this.yandexSay(text, { speaker: this.voice.voiceURI, speed: this.rate });
+        this.yandexSay(text, { speaker: this.voice.voiceURI, speed: this.rate }, download)
         return
       }
-      utterThis.voice = <SpeechSynthesisVoice>this.voice;
+      utterThis.voice = <SpeechSynthesisVoice>this.voice
       utterThis.pitch = this.pitch
       utterThis.rate = this.rate
       utterThis.volume = this.volume
 
       utterThis.onend = (event) => {
         this.events.emit('end')
-      };
-      utterThis.onerror = function (event) {
-
-      };
+      }
+      utterThis.onerror = function (event) {}
       this.synth.cancel()
-      this.synth.speak(utterThis);
+      this.synth.speak(utterThis)
     }
   }
-  async yandexSay(text: string, params: { speaker: string; speed: number; }) {
-    //good luck
-    const response = await axios.post("http://linka.su:5443/voice", {
-      text,
-      voice: params.speaker
-    }, {
-      responseType: 'arraybuffer'
-    })
+  async yandexSay(
+    text: string,
+    params: { speaker: string; speed: number },
+    download = false,
+  ) {
+    const response = await axios.post(
+      'https://us-central1-distypepro-android.cloudfunctions.net/tts',
+      {
+        text,
+        voice: params.speaker,
+      },
+      {
+        responseType: 'arraybuffer',
+      },
+    )
 
-    const blob = new Blob([response.data], { type: "audio/mp3" });
-    const url = window.URL.createObjectURL(blob);
-    this.audio.pause();
+    const blob = new Blob([response.data], { type: 'audio/mp3' })
+    const url = window.URL.createObjectURL(blob)
+
+    if (download) {
+      console.log('download');
+      
+      let link = document.createElement('a')
+      link.download = 'LINKa. напиши. '+text.substring(0, 5)+'.mp3'
+      link.href = url
+      link.click()
+
+      URL.revokeObjectURL(link.href)
+      this.events.emit('end')
+      return
+    }
+    this.audio.pause()
     this.audio.src = url
     await this.audio.play()
-    this.audio
-      .onended = () =>
-        this.events.emit('end')
-
+    this.audio.onended = () => this.events.emit('end')
   }
   stop() {
     if (this.yandex) {
@@ -76,27 +88,26 @@ class TTS {
   }
 
   get volume() {
-    return this.storage.getNumber('volume', 1);
-  };
+    return this.storage.getNumber('volume', 1)
+  }
   set volume(volume: number) {
-    this.storage.setNumber('volume', volume);
+    this.storage.setNumber('volume', volume)
   }
   get rate() {
-    return this.storage.getNumber('rate', 1);
-  };
+    return this.storage.getNumber('rate', 1)
+  }
   set rate(rate: number) {
-    this.storage.setNumber('rate', rate);
+    this.storage.setNumber('rate', rate)
   }
   get pitch() {
-    return this.storage.getNumber('pitch', 1);
-  };
+    return this.storage.getNumber('pitch', 1)
+  }
   set pitch(pitch: number) {
-    this.storage.setNumber('pitch', pitch);
+    this.storage.setNumber('pitch', pitch)
   }
   get yandex() {
     const value = !!this.storage.getBoolean('yandex', false)
-    analytics()
-      .setUserProperties({ use_yandex: value })
+    analytics().setUserProperties({ use_yandex: value })
 
     return value
   }
@@ -105,12 +116,13 @@ class TTS {
   }
 
   setVoice(uri: string) {
+    const voice =
+      this.offlineVoices.find((item) => item.voiceURI === uri) ||
+      this.yandexVoices.find((item) => item.voiceURI === uri)
 
-    const voice = this.offlineVoices.find(item => item.voiceURI === uri) || this.yandexVoices.find(item => item.voiceURI === uri)
-
-    if (!voice) return;
+    if (!voice) return
     this.storage.setString('voiceuri', voice.voiceURI)
-    this.voice = voice;
+    this.voice = voice
   }
 
   get offlineVoices(): SpeechSynthesisVoice[] {
@@ -118,48 +130,44 @@ class TTS {
   }
   get yandexVoices(): YandexVoice[] {
     return [
-      { voiceURI: "zahar", text: "Захар" },
-      { voiceURI: "ermil", text: "Емиль" },
-      { voiceURI: "jane", text: "Джейн" },
-      { voiceURI: "oksana", text: "Оксана" },
-      { voiceURI: "alena", text: "Алёна" },
-      { voiceURI: "filipp", text: "Филипп" },
-      { voiceURI: "omazh", text: "Ома" }
+      { voiceURI: 'zahar', text: 'Захар' },
+      { voiceURI: 'ermil', text: 'Емиль' },
+      { voiceURI: 'jane', text: 'Джейн' },
+      { voiceURI: 'oksana', text: 'Оксана' },
+      { voiceURI: 'alena', text: 'Алёна' },
+      { voiceURI: 'filipp', text: 'Филипп' },
+      { voiceURI: 'omazh', text: 'Ома' },
     ]
   }
   get defaultOfflineVoice(): SpeechSynthesisVoice {
-
-
-    return this.offlineVoices.find(item => item.default) || this.offlineVoices[0]
+    return (
+      this.offlineVoices.find((item) => item.default) || this.offlineVoices[0]
+    )
   }
 
   get selectedVoice(): SpeechSynthesisVoice | YandexVoice {
-
-    const def = this.defaultOfflineVoice;
-    const voices = this.offlineVoices;
-    const uri = this.storage.getString('voiceuri', def ? def.voiceURI : "uri")
+    const def = this.defaultOfflineVoice
+    const voices = this.offlineVoices
+    const uri = this.storage.getString('voiceuri', def ? def.voiceURI : 'uri')
     if (this.yandex) {
-      const voice = this.yandexVoices.find(voice => voice.voiceURI === uri);
-      analytics()
-        .setUserProperties({ voice: voice!.voiceURI });
+      const voice = this.yandexVoices.find((voice) => voice.voiceURI === uri)
+      analytics().setUserProperties({ voice: voice!.voiceURI })
       return voice || this.yandexVoices[0]
     }
-    const voice = voices.find(voice => voice.voiceURI === uri)
-
+    const voice = voices.find((voice) => voice.voiceURI === uri)
 
     if (!voice && def) {
-      this.storage.setString('voiceuri', def ? def.voiceURI : "uri")
+      this.storage.setString('voiceuri', def ? def.voiceURI : 'uri')
       return this.selectedVoice
     }
-    analytics()
-      .setUserProperties({ voice: voice!.voiceURI })
+    analytics().setUserProperties({ voice: voice!.voiceURI })
     return voice!
   }
 }
-let instance: TTS = new TTS;
-export default TTS;
+let instance: TTS = new TTS()
+export default TTS
 
 interface YandexVoice {
-  voiceURI: string,
+  voiceURI: string
   text: string
 }
